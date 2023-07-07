0403f00bb1777f53d10d1cf5b2fd77356b9ef6cb
Denis Miagkov <tomich-84@mail.ru>
2023-07-07 14:29:04 +0200

    first commit

---
+++ part1.sql
@@ -0,0 +1,8 @@
+create table personal_information
+(
+	customer_id INT primary key,
+	customer_name VARCHAR(45),
+	customer_surname VARCHAR(45),
+	customer_primary_email VARCHAR(45),
+	customer_primary_phone VARCHAR(15)
+);
