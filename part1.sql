32e613847ed37ce29d5510923ee9054d3fbff9c4
Denis Miagkov <tomich-84@mail.ru>
2023-07-07 15:38:19 +0200

    personal_information check implemented

--- part1.sql
+++ part1.sql
@@ -1,8 +1,9 @@
-create table personal_information
+create table personal_information3
 (
-	customer_id INT primary key,
-	customer_name VARCHAR(45),
-	customer_surname VARCHAR(45),
-	customer_primary_email VARCHAR(45),
-	customer_primary_phone VARCHAR(15)
+	customer_id SERIAL primary key,
+	customer_name VARCHAR(25)check(substring(customer_name, 1, 1) = upper(substring(customer_name, 1, 1))),
+	customer_surname VARCHAR(25)check(substring(customer_surname, 1, 1) = upper(substring(customer_surname, 1, 1))),
+	customer_primary_email VARCHAR(25) check (customer_primary_email like '%@%'),
+	customer_primary_phone VARCHAR(12) check (customer_primary_phone like '+7__________')
 );
+
