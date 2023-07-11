acae7147082b221b3824e7e5b4f777042cc26ce4
Denis Miagkov <tomich-84@mail.ru>
2023-07-11 12:32:33 +0200

    view purchase_history added

--- part2_purchase_history.sql
+++ part2_purchase_history.sql
@@ -4,9 +4,9 @@ with g as
 as group_sum, sum(sku_sum_paid) as group_sum_paid from checks c join 
 product_grid pg on c.sku_id = pg.sku_id 
 group by c.transaction_id, pg.group_id)
-select pi12.customer_id , t.transaction_id , t.transaction_datetime, g.group_id, g.group_cost, g.group_sum, 
+select cd.customer_id , t.transaction_id , t.transaction_datetime, g.group_id, g.group_cost, g.group_sum, 
 g.group_sum_paid
-from personal_information pi12 join
-cards c on pi12.customer_id = c.customer_id join transactions t on c.customer_card_id = t.customer_card_id join 
+from cards cd join transactions t on cd.customer_card_id = t.customer_card_id join 
 g on t.transaction_id = g.transaction_id
 order by customer_id, transaction_id, group_id;
+
